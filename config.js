4d8f013d9aaa062c19366e58bc0c90b1b5b4ba80
Haofan Zhang <hzhang3@wpi.edu>
2017-10-11 18:24:04 -0400

    trying

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ config = {
         client_id: 'f067d80090d34e11b883b81236ad9316',
         client_secret: '31786c2f0ef644fcaf91f15b0a1078d6',
         server: {
-            host: '127.0.0.1',
+            host: 'https://final-hzhang3atwpi.herokuapp.com/',
             port: '5000'
             // port: '3000'
         }
@@ -19,7 +19,7 @@ config = {
         client_id: 'f067d80090d34e11b883b81236ad9316',
         client_secret: '31786c2f0ef644fcaf91f15b0a1078d6',
         server: {
-            host: '127.0.0.1',
+            host: 'https://final-hzhang3atwpi.herokuapp.com/',
             port: '5000'
             // port: '3000'
         }
