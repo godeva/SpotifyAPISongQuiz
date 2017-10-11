63f1d67e9b621b899cb116b3fab8ff2cfd2d37f4
Haofan Zhang <hzhang3@wpi.edu>
2017-10-11 18:15:55 -0400

    heroku redirect

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var path = require('path'),
 
 config = {
     production: {
-        redirect_uri: 'http://localhost:5000/callback',
+        redirect_uri: 'https://final-hzhang3atwpi.herokuapp.com/',
         // redirect_uri: 'http://localhost:3000/callback',
         client_id: 'f067d80090d34e11b883b81236ad9316',
         client_secret: '31786c2f0ef644fcaf91f15b0a1078d6',
@@ -14,7 +14,7 @@ config = {
         }
     },
     development: {
-      redirect_uri: 'http://localhost:5000/callback',
+      redirect_uri: 'https://final-hzhang3atwpi.herokuapp.com/callback',
         // redirect_uri: 'http://localhost:3000/callback',
         client_id: 'f067d80090d34e11b883b81236ad9316',
         client_secret: '31786c2f0ef644fcaf91f15b0a1078d6',
